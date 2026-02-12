4419ad3caaadce715b64befa373ce323d66e6dea
Suyash Upadhyay <66892274+Sash107@users.noreply.github.com>
2026-02-12 17:43:43 +0530

    Update db.js

--- type-1/src/db/db.js
+++ type-1/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 
 async function connectDB(){
-    await mongoose.connect("mongodb+srv://learner:dgNZssMCwWvEa5r6@cluster0.h3tge2x.mongodb.net/randomm");
+    await mongoose.connect("");
     console.log("DB connected");
 }
 
